235440713f3d0f359831a179fd35d1aac480238f
Xue88 <xue.zhao@calibresys.com>
2016-10-11 09:52:59 -0400

    test Jenkins

--- TestGradle/src/test/java/LibraryTest.java
+++ TestGradle/src/test/java/LibraryTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-      
+      //test Jenkins
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
